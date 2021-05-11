d41464e27f36aae0e927c223f984fbc1ef1be61a
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-11 00:59:11 -0700

    [Charan] Add : User-Name Validation.

---
+++ UserValidations/UserValidations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserValidations
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Name :");
+            String first_name = (Console.ReadLine());
+            if (first_name.Length >= 3)
+            { 
+                Regex reg = new Regex(@"^[A-Z]");
+                Match match = reg.Match(first_name);
+                if(match.Success)
+                    Console.WriteLine("Valid User Name");
+                else
+                    Console.WriteLine("Invalid User Name");
+            }
+            else
+                Console.WriteLine("Invalid User Name");
+        }
+    }
+}
