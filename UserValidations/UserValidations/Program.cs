edd15e688886ea8e71b5e402dbf4ae31b0357cb5
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-13 11:48:47 -0700

    [Charan] Add: Password Greater than 8 Characters

--- UserValidations/UserValidations/Program.cs
+++ UserValidations/UserValidations/Program.cs
@@ -3,23 +3,18 @@ using System.Text.RegularExpressions;
 
 namespace UserValidations
 {
-    class ValidEmail
+    class Password
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Phone Number :");
-            String Ph_no = (Console.ReadLine());
-            if (Ph_no.Length == 13)
-            { 
-                Regex reg = new Regex(@"^(91)[ ]{1}[7-9]{1}[0-9]{9}");
-                Match match = reg.Match(Ph_no);
-                if(match.Success)
-                    Console.WriteLine("" +Ph_no + " is" + " Valid Phone Number");
-                else
-                    Console.WriteLine("" +Ph_no + " is" + " Invalid Number");
+            Console.WriteLine("Enter Password:");
+            string password = (Console.ReadLine());
+            if (password.Length >= 8)
+            {
+                Console.WriteLine("Valid Password");
             }
             else
-                Console.WriteLine("" +Ph_no + " is" + " Invalid Phone Number");
+                Console.WriteLine("invalid input ");
         }
     }
 }
