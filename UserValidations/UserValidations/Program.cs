b408fcfcb6a706568db1727b4b73f9b36ae50a66
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-13 12:53:28 -0700

    [Charan] Add: Password Must Uppercase Lowercase Special and Number

--- UserValidations/UserValidations/Program.cs
+++ UserValidations/UserValidations/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace UserValidations
+namespace UserPassword
 {
     class Password
     {
@@ -10,11 +10,16 @@ namespace UserValidations
             Console.WriteLine("Enter Password:");
             string password = (Console.ReadLine());
             if (password.Length >= 8)
-            {
-                Console.WriteLine("Valid Password");
+            { 
+                Regex reg = new Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$)");
+                Match match = reg.Match(password);
+                if (match.Success)
+                        Console.WriteLine("" + password + " is" + " Valid Password");
+                else
+                        Console.WriteLine("" + password + " is" + " Invalid Password");
             }
             else
-                Console.WriteLine("invalid input ");
+                Console.WriteLine(""+ password + " is" + " Invalid Password");
         }
     }
 }
